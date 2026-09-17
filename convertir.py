5dfabd7c6577bb1ed77f076c1bbcd667f5255488
Samuel Ticona <samuelticona085@gmail.com>
2026-09-17 19:23:17 -0400

    Agregar programa de conversión de temperatura

---
+++ convertir.py
@@ -0,0 +1,7 @@
+print ( " *********************************************************** " )
+print ( " *== Convertir temperatura de grados Celsius a Farenheit ==* " )
+print ( " *********************************************************** " )
+print ( "" )
+cel = float ( input ( " ingrese el dato para hacer la conversion: " ) )
+result = ( cel * 1.8 ) + 32
+print ( " la conversion es: ", result)
