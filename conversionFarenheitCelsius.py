572370bdbb842bd8769bd86fc3f7f0cc5d2dcfac
Samuel Ticona <samuelticona085@gmail.com>
2026-09-17 19:43:28 -0400

    Add Fahrenheit to Celsius conversion program

---
+++ conversionFarenheitCelsius.py
@@ -0,0 +1,7 @@
+print  ( " ********************************************************************** " )
+print  ( " *== Programa para convertir de grados Farenheit a Celsius ==* " )
+print  ( " ********************************************************************** " )
+print  ( " " )
+far = int ( input (  " ingrese el grado para convertir: " ) )
+conver = ( far - 32 ) / 1.8
+print ( " el resultado que esperabas es: ", conver )
