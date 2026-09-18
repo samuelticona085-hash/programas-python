fc6d177710b16d02bb2d8ac661f4248f8f56488d
Samuel Ticona <samuelticona085@gmail.com>
2026-09-17 20:58:47 -0400

    Add program to calculate triangle area

---
+++ areaTriangulo.py
@@ -0,0 +1,8 @@
+print ( " ************************************************************* " )
+print ( " *== Programa para calcular el area de un teiangulo ==* " )
+print ( " ************************************************************* " )
+print ( " " )
+a = float ( input ( " ingrese el dato de la base: " ) )
+b = float ( input ( " ingrese el dato de la altura: " ) )
+area = ( a * b ) / 2
+print ( " el area de tu triangulo es: ", area )
