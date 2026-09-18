056e8b36d4e5aa5c6a9c5e85471eda3405754961
Samuel Ticona <samuelticona085@gmail.com>
2026-09-17 21:23:26 -0400

    Add average grade calculation program
    Implement program to calculate average of grades and determine pass/fail status.

---
+++ promedioNotas.py
@@ -0,0 +1,14 @@
+print ( " ======================================= " )
+print ( " ** Programa para calcular el promedio de tus notas ** " )
+print ( " ======================================= " )
+print ( " " )
+a = float ( input ( " ingrese la primera nota: " ) )
+b = float ( input ( " ingrese la segunda nota: " ) )
+c = float ( input ( " ingrese la tercera nota: " ) )
+promedio = ( a + b + c ) / 3
+print ( " el promedio de tus notas es: ", promedio )
+if promedio >= 50 :
+    print ( " aprobaste " )
+else :
+    print ( " reprobaste T-T " )
+    
