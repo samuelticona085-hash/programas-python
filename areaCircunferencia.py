74c009bc2946118ccedc3e9724c5fc15c81409d4
Samuel Ticona <samuelticona085@gmail.com>
2026-09-17 21:09:56 -0400

    Create areaCircunferencia.py

---
+++ areaCircunferencia.py
@@ -0,0 +1,8 @@
+import math
+print ( " ************************************************************* " )
+print ( " *== Programa para calcular el area de una circunferencia ==* " )
+print ( " ************************************************************* " )
+print ( " " )
+a = float ( input ( " ingrese el radio: " ) )
+area = math.pow ( a, 2) * math.pi
+print ( " el area de tu triangulo es: ", area )
