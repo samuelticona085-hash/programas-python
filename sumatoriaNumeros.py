f0691a57f51c036eac34042075f2e3d96235549a
Samuel Ticona <samuelticona085@gmail.com>
2026-09-25 15:37:33 -0400

    Add sumatoriaNumeros.py to calculate sum of 10 numbers

---
+++ sumatoriaNumeros.py
@@ -0,0 +1,8 @@
+print ( " ********************************* " )
+print ( " * == Sumaoriqa de 10 numeros == * " )
+print ( " ********************************* " )
+print ( " " )
+suma = 0
+for i in range ( 1, 11 ):
+    suma += i
+    print (suma)
