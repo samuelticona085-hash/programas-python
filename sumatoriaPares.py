3cc7a50d96a36a6b4f5efafc177372a13176178a
Samuel Ticona <samuelticona085@gmail.com>
2026-09-25 16:25:42 -0400

    Add script to calculate sum of even numbers

---
+++ sumatoriaPares.py
@@ -0,0 +1,8 @@
+print ( " ***************************** " )
+print ( " * == Sumatroria de pares == * " )
+print ( " ***************************** " )
+print ( " " )
+suma = 0
+for i in range ( 2, ( 20  * 2 ) + 1, 2 ):
+    suma += i
+print (suma)
